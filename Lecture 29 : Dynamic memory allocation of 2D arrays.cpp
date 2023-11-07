194743397421970ba40eeea5d8bd50dc6cacb84d
Souradip Ghosh <84944565+Souradip2000@users.noreply.github.com>
2023-11-07 17:16:37 +0530

    Update and rename Lecture 29 : Dynamic memory allocation of 2D arrays to Lecture 29 : Dynamic memory allocation of 2D arrays.cpp

--- Lecture 29 : Dynamic memory allocation of 2D arrays.cpp
+++ Lecture 29 : Dynamic memory allocation of 2D arrays.cpp
@@ -1,3 +1,66 @@
+// My code
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, m;
+    cout<<"Enter the number of rows and cols: ";
+    cin>>n>>m;
+    
+    int* * arr = new int*[n];
+    for(int i=0;i<n;i++){
+        arr[i]=new int[m];
+    }
+    
+    cout<<endl<<"Enter the elements of the 2D array: "<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+    
+    cout<<"Displaying the 2D array: "<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    for(int i=0;i<n;i++){
+        delete []arr[i];
+    }
+    
+    delete []arr;
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+________________________________________________________________________________________________________________________________________________________
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 include <iostream>
 using namespace std;
 
